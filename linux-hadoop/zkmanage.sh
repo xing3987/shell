18ce30fefd73d44019490818e11585476431e294
chenxing <xing3987@163.com>
2019-03-30 00:05:46 +0800

    2019-3-30

---
+++ linux-hadoop/zkmanage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+for host in hadoop001 hadoop002 hadoop003
+do 
+	echo $host ${1}"ing..."
+	ssh $host "source /etc/profile;/data/hadoop/zookeeper-3.4.10/bin/zkServer.sh $1"
+done
+
+sleep 2
+
+for host in hadoop001 hadoop002 hadoop003
+do
+	echo "*********** ${host} status: ************"
+	ssh $host "source /etc/profile;/data/hadoop/zookeeper-3.4.10/bin/zkServer.sh status"
+done
+
